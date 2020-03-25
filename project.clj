15ebf6fad3852f8c9574d21431fb5ea709bfdc2b
Aphyr <aphyr@aphyr.com>
2020-03-25 17:47:17 -0400

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elle "0.1.0-SNAPSHOT"
+(defproject elle "0.1.0"
   :description "Black-box transactional consistency checker, based on cycle detection"
   :url "https://github.com/jepsen-io/elle"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -8,7 +8,7 @@
                  [knossos "0.3.6"]
                  [org.clojure/tools.logging "0.6.0"]
                  [rhizome "0.2.9"]
-                 [jepsen.txn "0.1.2-SNAPSHOT"]
+                 [jepsen.txn "0.1.2"]
                  [io.lacuna/bifurcan "0.1.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :repl-options {:init-ns elle.core}
