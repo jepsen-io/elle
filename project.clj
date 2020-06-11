20e1d9baca7e91258ae156ea0694ecd516b8ba10
Aphyr <aphyr@aphyr.com>
2020-06-11 09:10:25 -0400

    knossos 0.3.7, elle 0.1.2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject elle "0.1.2-SNAPSHOT"
+(defproject elle "0.1.2"
   :description "Black-box transactional consistency checker, based on cycle detection"
   :url "https://github.com/jepsen-io/elle"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[slingshot "0.12.2"]
                  [dom-top "1.0.5"]
-                 [knossos "0.3.6"
+                 [knossos "0.3.7"
                   :exclusions [org.slf4j/slf4j-log4j12]]
                  [org.clojure/tools.logging "0.6.0"]
                  [rhizome "0.2.9"]
