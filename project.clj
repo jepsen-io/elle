4e6c2907a62baa0be115b91496eee41370dc51e2
Aphyr <aphyr@aphyr.com>
2021-11-03 13:44:17 -0400

    Upgrade tools.logging, unilog, clojure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elle "0.1.3-SNAPSHOT"
+(defproject elle "0.1.3"
   :description "Black-box transactional consistency checker, based on cycle detection"
   :url "https://github.com/jepsen-io/elle"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -8,12 +8,12 @@
                  [hiccup "1.0.5"]
                  [knossos "0.3.7"
                   :exclusions [org.slf4j/slf4j-log4j12]]
-                 [org.clojure/tools.logging "0.6.0"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [rhizome "0.2.9"]
                  [jepsen.txn "0.1.2"]
                  [io.lacuna/bifurcan "0.1.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
-                                  [spootnik/unilog "0.7.24"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]
+                                  [spootnik/unilog "0.7.28"]]}}
   :jvm-opts ["-server"
              ;"-XX:-OmitStackTraceInFastThrow"
              ;"-XX:+PrintGC"
