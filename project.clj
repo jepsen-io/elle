02f74561072008a0254dc5e1aa4b25e472b61b9b
Aphyr <aphyr@aphyr.com>
2022-11-02 08:24:17 -0400

    Dom-top 1.0.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[slingshot "0.12.2"]
-                 [dom-top "1.0.7"]
+                 [dom-top "1.0.8-SNAPSHOT"]
                  [hiccup "1.0.5"]
                  [knossos "0.3.8"
                   :exclusions [org.slf4j/slf4j-log4j12]]
