bd9535371982fedf3e28541c8ea76509f1d4bf28
Aphyr <aphyr@aphyr.com>
2022-12-05 22:41:22 -0500

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject elle "0.1.6-SNAPSHOT"
+(defproject elle "0.1.6"
   :description "Black-box transactional consistency checker, based on cycle detection"
   :url "https://github.com/jepsen-io/elle"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[slingshot "0.12.2"]
-                 [dom-top "1.0.8-SNAPSHOT"]
+                 [dom-top "1.0.8"]
                  [hiccup "1.0.5"]
                  [org.clojure/tools.logging "1.2.4"]
                  [rhizome "0.2.9"]
-                 [io.jepsen/history "0.1.0-SNAPSHOT"]
+                 [io.jepsen/history "0.1.0"]
                  [jepsen.txn "0.1.2"]
                  [io.lacuna/bifurcan "0.2.0-alpha6"]]
   :java-source-paths ["src"]
