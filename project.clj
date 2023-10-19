d8d4b39b29716d5a83f1941cfa1a1af5662b0c68
Aphyr <aphyr@aphyr.com>
2023-10-18 21:53:59 -0500

    jepsen.history 0.1.2, dom-top 1.0.9

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[slingshot "0.12.2"]
-                 [dom-top "1.0.9-SNAPSHOT"]
+                 [dom-top "1.0.9"]
                  [hiccup "1.0.5"]
                  [org.clojure/tools.logging "1.2.4"]
                  [rhizome "0.2.9"]
-                 [io.jepsen/history "0.1.1"]
+                 [io.jepsen/history "0.1.2"]
                  [jepsen.txn "0.1.2"]
                  [io.lacuna/bifurcan "0.2.0-alpha6"]]
   :java-source-paths ["src"]
