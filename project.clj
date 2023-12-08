e5f23c5b97d458a1244ff223e98b4bcc740bb185
Aphyr <aphyr@aphyr.com>
2023-12-07 21:41:12 -0600

    Dep upgrades
    bifurcan-clj 0.1.1
    jepsen.history 0.1.3
    jepsen.history.sim 0.1.0
    
    Drop unused java compilation experiment

--- project.clj
+++ project.clj
@@ -4,24 +4,24 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[slingshot "0.12.2"]
-                 [com.aphyr/bifurcan-clj "0.1.1-SNAPSHOT"]
+                 [com.aphyr/bifurcan-clj "0.1.1"]
                  ; Has to be here for our java classes to use Clojure
                  [org.clojure/clojure "1.11.1"]
                  [dom-top "1.0.9"]
                  [hiccup "1.0.5"]
                  [org.clojure/tools.logging "1.2.4"]
                  [rhizome "0.2.9"]
-                 [io.jepsen/history "0.1.3-SNAPSHOT"]
+                 [io.jepsen/history "0.1.3"]
                  [jepsen.txn "0.1.2"]]
   :java-source-paths ["src"]
   ; We need jepsen.history.Op available before we can compile our java code
-  :prep-tasks [["compile" "jepsen.history"]
-               "javac"
-               "compile"]
+  ;:prep-tasks [["compile" "jepsen.history"]
+  ;             "javac"
+  ;             "compile"]
   :javac-options ["-target" "1.8" "-source" "1.8"
                   ]
   :profiles {:dev {:dependencies [[com.gfredericks/test.chuck "0.2.14"]
-                                  [io.jepsen/history.sim "0.1.0-SNAPSHOT"]
+                                  [io.jepsen/history.sim "0.1.0"]
                                   [org.clojure/test.check "1.1.1"]
                                   [spootnik/unilog "0.7.31"]]}}
   :jvm-opts ["-server"
